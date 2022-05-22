a66c8b70e7b16017376cd44e982adff80d37bd74
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-22 16:09:08 +0600

    mongodb conneted

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
@@ -8,6 +9,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//mongodb
+const uri =
+  "mongodb+srv://doctor_admin:<password>@cluster0.nosty.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Server Running to the Doctor");
 });
