3398243d8d2a3d0eede85924a4cbc8b6b0f13b60
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-22 15:53:43 +0600

    server init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server Running to the Doctor");
+});
+app.listen(port, () => {
+  console.log("DOCTORS APP", port);
+});
