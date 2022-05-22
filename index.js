0362814f86895a4d5a385b6bdf31c03eda26edf3
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-22 16:38:48 +0600

    updated

--- index.js
+++ index.js
@@ -10,21 +10,27 @@ app.use(cors());
 app.use(express.json());
 
 //mongodb
-const uri =
-  "mongodb+srv://doctor_admin:<password>@cluster0.nosty.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nosty.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 async function run() {
   try {
+    await client.connect();
+    const servicesCollection = client
+      .db("doctors_portal")
+      .collection("services");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
   } finally {
   }
 }
