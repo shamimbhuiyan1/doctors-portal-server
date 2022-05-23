d5b521b5d08254309b4853f8b8a89c3a4f583a9f
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-23 18:56:04 +0600

    available,booking post created

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware
 app.use(cors());
 app.use(express.json());
 
-//mongodb
+//IST database user info
+//DB_USER=doctor_admin
+//DB_PASS=6HOVlIYO42hIMXub
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nosty.mongodb.net/?retryWrites=true&w=majority`;
-/* console.log(uri); */
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -20,58 +23,96 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    //for all servics
     await client.connect();
-    const servicesCollection = client
+    const serviceCollection = client
       .db("doctors_portal")
       .collection("services");
-
-    //for booking
     const bookingCollection = client
       .db("doctors_portal")
       .collection("bookings");
 
-    //for getting all data
     app.get("/service", async (req, res) => {
       const query = {};
-      const cursor = servicesCollection.find(query);
+      const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
 
-      /**
-       * API Naming Convention
-       * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
-       * app.get('/booking/:id') // get a specific booking
-       * app.post('/booking') // add a new booking
-       * app.patch('/booking/:id) //
-       * app.delete('/booking/:id) //
-       *
-       */
+    // Warning: This is not the proper way to query multiple collection.
+    // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
 
-      app.post("/booking", async (req, res) => {
-        const booking = req.body;
-        //ekoi din patient ekadik appoinment jate na korte pare se jonno nicher vabe korbo.
-        const query = {
-          treatment: booking.treatment,
-          date: booking.date,
-          patient: booking.patient,
-        };
-        const exists = await bookingCollection.findOne(query);
-        if (exists) {
-          return res.send({ success: false, booking: exists });
-        }
-        const result = await bookingCollection.insertOne(booking);
-        return res.send({ success: true, result });
+      // step 1:  get all services
+      const services = await serviceCollection.find().toArray();
+
+      // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      // step 3: for each service
+      services.forEach((service) => {
+        // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+        const serviceBookings = bookings.filter(
+          (book) => book.treatment === service.name
+        );
+
+        // step 5: select slots for the service Bookings: ['', '', '', '']
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+
+        // step 6: select those slots that are not in bookedSlots
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+
+        //step 7: set available to slots to make it easier
+        service.slots = available;
       });
+
+      res.send(services);
+    });
+
+    /**
+     * API Naming Convention
+     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+     * app.get('/booking/:id') // get a specific booking
+     * app.post('/booking') // add a new booking
+     * app.patch('/booking/:id) //
+     * app.delete('/booking/:id) //
+     */
+
+    app.get("/booking", async (req, res) => {
+      const patient = req.query.patient;
+      const query = { patient: patient };
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
+    //ekoi din patient ekadik appoinment jate na korte pare se jonno nicher vabe korbo.
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
     });
   } finally {
   }
 }
 
 run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Server Running to the Doctor");
+  res.send("Hello From Doctor Uncle!");
 });
+
 app.listen(port, () => {
-  console.log("DOCTORS APP", port);
+  console.log(`Doctors App listening on port ${port}`);
 });
